1e492acc2531f305bbddbff140e96984c362b1a0
Matthew Murphy <murphyma@grinnell.edu>
2017-10-24 14:27:07 +0000

    add base api route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   resources :farms
 
   root :to => redirect('/farms')
+
+  # define api get for json data
+  scope '/api' do
+    scope '/v1' do
+      scope '/farms' do
+        get '/' => 'farms#index'
+      end
+    end
+  end
+
 end
