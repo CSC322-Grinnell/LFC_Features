983c805efad8e45ef209ac2018bc301db1ca9799
Minh Tran <tranminh@grinnell.edu>
2017-11-02 14:31:25 +0000

    Add header and CORS for API endpoint

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,4 +1,6 @@
 class FarmsController < ApplicationController
+  
+  before_action :authenticate
 
   def index
     @farms = Farm.all
@@ -8,6 +10,15 @@ class FarmsController < ApplicationController
 
   def buildJson
     render :json => Farm.all
+  end
+  
+  
+  protected
+  def authenticate
+    puts "haha"
+    authenticate_with_http_token do |token, options|
+      return @token = token
+    end
   end
 
 end
