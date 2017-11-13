5cd6edd53e043749b8eccea5f06a462520bf1bd4
murphymatt <murphyma@grinnell.edu>
2017-11-13 02:30:01 -0600

    add review components in controller and view

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,28 +1,28 @@
 class FarmsController < ApplicationController
-  
-  #before_action :authenticate
+
+  before_action :authenticate
   def show
-    
+
   end
 
-  
+
   def index
     @farms = Farm.all
   end
-  
+
   def new
     @farm = Farm.new
   end
-  
+
   def create
     @farm = Farm.new(farm_params)
     if @farm.save
       redirect_to @farm
     else
       render :new
-    end 
+    end
   end
-  
+
   def apiJson
     @farms = Farm.all
     @farms_json = buildJson
@@ -32,12 +32,13 @@ class FarmsController < ApplicationController
   def buildJson
     render :json => Farm.all
   end
-  
-  def jsonData
-    apiJson
+
+  def review
+    @invalid_farms = nil
+    @invalid_farms = Farm.where(approved: false)
+    render :json => @invalid_farms
   end
-  
-  
+
   protected
   def authenticate
     puts "haha"
@@ -45,13 +46,13 @@ class FarmsController < ApplicationController
       return @token = token
     end
   end
-  
+
   #private
   def farm_params
     params.require(:farm).permit(:name, :address, :url, :phone, :facebook, :instagram, :twitter)
   end
-  
-  
+
+
 
 
 end
