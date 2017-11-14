74c1716ea1ff325cb3535e250f2c6be0f1fd49b9
murphymatt <murphyma@grinnell.edu>
2017-11-13 23:34:43 -0600

    add approval/rejection buttons

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :farms
   root :to => redirect('/farms')
   get 'farms/review'
+  post 'approve' => 'farms#approve', as: :approve
+  post 'reject' => 'farms#reject', as: :reject
 
   # define api get for json data
   scope '/api' do
