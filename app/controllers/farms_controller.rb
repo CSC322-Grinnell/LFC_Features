c9f95c38bc20337080fbb2367ad1dac6e42ec4aa
murphymatt <murphyma@grinnell.edu>
2017-11-13 21:48:18 -0600

    add haml to gemfile

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -2,27 +2,39 @@ class FarmsController < ApplicationController
 
   before_action :authenticate
 
-  def index
-    #@show_farms = Farm.where(approved: true)
-    #@farms_json = buildJson
-    #puts @farms_json
-
-    review
+  def farm_params
+    params.require(:farm)
+        .permit(:name, :address, :url, :phone, :facebook, :instagram, :twitter)
   end
 
-  def buildJson
-    render :json => Farm.all
+  def show
+    id = params[:id]
+    @farm = Farm.find(id)
+  end
+
+  def index
+    @show_farms = Farm.where(approved: true)
+    @farms_json = buildJson @show_farms
+    # puts @farms_json
   end
 
   def review
-    @invalid_farms = nil
     @invalid_farms = Farm.where(approved: false)
-    render :json => @invalid_farms
+    @review_json = buildJson @invalid_farms
+    # puts @review_json
+  end
+
+  def apiJson
+    @show_farms = Farm.where(approved: true)
+    @farms_json = buildJson @show_farms
+  end
+
+  def buildJson farms
+    render :json => farms
   end
 
   protected
   def authenticate
-    puts "haha"
     authenticate_with_http_token do |token, options|
       return @token = token
     end
