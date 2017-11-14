6d83493f922b984a91d68d8fe4c94f50998887d3
murphymatt <murphyma@grinnell.edu>
2017-11-13 23:19:14 -0600

    migrate html to show

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :farms
   root :to => redirect('/farms')
+  get 'farms/review'
 
   # define api get for json data
   scope '/api' do
     scope '/v1' do
       scope '/farms' do
-        get '/' => 'farms#index'
+        get '/' => 'farms#apiJson'
       end
     end
   end
