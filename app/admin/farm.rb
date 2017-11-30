12b6b4666c7be3d1e44ccbeae09357536d66da11
murphymatt <murphyma@grinnell.edu>
2017-11-30 08:53:10 -0600

    modify AA view

--- app/admin/farm.rb
+++ app/admin/farm.rb
@@ -10,5 +10,46 @@ ActiveAdmin.register Farm do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
-# end
+  # end
+
+  permit_params do
+    permitted = [:name, :address, :phone, :email, :url, :facebook, :instagram, :twitter, :password, :password_confirmation]
+    if params[:action] == 'edit'
+      puts "params[:action] == edit"
+    end
+    if current_user.admin?
+      puts "current_user.admin?"
+    end
+    permitted << :approved if params[:action] == 'edit' && current_user.admin?
+    permitted
+  end
+
+  index do
+    column :name
+    column :email
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    actions
+  end
+
+  filter :email
+
+  form do |f|
+    f.inputs "Farm Details" do
+      f.input :name
+      f.input :address
+      f.input :phone
+      f.input :email
+      f.input :url
+
+      f.input :facebook
+      f.input :instagram
+      f.input :twitter
+
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
