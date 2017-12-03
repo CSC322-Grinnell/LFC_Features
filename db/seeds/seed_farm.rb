b4e6980906ffa7815abbb1cf6fcc73869b5f7e52
murphymatt <murphyma@grinnell.edu>
2017-12-03 17:50:56 -0600

    add pundit func

--- db/seeds/seed_farm.rb
+++ db/seeds/seed_farm.rb
@@ -7,12 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Farm.create!(email: 'admin@example.com', password: 'password',
-             password_confirmation: 'password') if Rails.env.development?
+             password_confirmation: 'password', role: 0) \
+            if Rails.env.development?
 
 farm_list = [
-    ["Uncle Bill's Farm", "123 Broad St.", "unclebill.com", "515123456", "Uncle Bill's Farm", "uncle_bill_farm", "uncle_bill_farm", true, "unclebill@example.com", "ubpassword", "ubpassword"],
-    ["Uncle Joe's Farm", "1205 Main St.", "unclejoe.com", "641234123", "", "uncle_joe_farm", "", true, "unclejoe@example.com", "ujpassword", "ujpassword"],
-    ["Aunt Maria's Farm", "1641 Park St.", "auntmaria.com", "5150009999", "Aunt Maria's Farm", "auntmaria_farm", "auntmaria_farm", false, "auntmaria@example.com", "ampassword", "ampassword"]
+    ["Uncle Bill's Farm", "123 Broad St.", "unclebill.com", "515123456", \
+     "Uncle Bill's Farm", "uncle_bill_farm", "uncle_bill_farm", true, \
+     "unclebill@example.com", "ubpassword", "ubpassword", 0],
+    ["Uncle Joe's Farm", "1205 Main St.", "unclejoe.com", "641234123", "", \
+     "uncle_joe_farm", "", true, "unclejoe@example.com", "ujpassword", \
+     "ujpassword", 0],
+    ["Aunt Maria's Farm", "1641 Park St.", "auntmaria.com", "5150009999", \
+     "Aunt Maria's Farm", "auntmaria_farm", "auntmaria_farm", false, \
+     "auntmaria@example.com", "awpassword", "awpassword", 0]
 ]
 
 farm_list.each do |name, address, url, phone, facebook, instagram, twitter, approved, email, password, password_confirmation|
