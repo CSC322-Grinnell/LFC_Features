548670c033b0cee858b506313e78abaf1dbfc1f5
murphymatt <murphyma@grinnell.edu>
2017-12-04 23:33:24 -0600

    remove merge artifacts

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,19 +2,12 @@ class Farm < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 512133238165cab75fd755e3924ff5dd6870492d
-    has_and_belongs_to_many :operations
-    has_and_belongs_to_many :growing_methods
-    has_and_belongs_to_many :selling_methods
-    has_and_belongs_to_many :markets
+       
+  has_and_belongs_to_many :operations
+  has_and_belongs_to_many :growing_methods
+  has_and_belongs_to_many :selling_methods
+  has_and_belongs_to_many :markets
 
 end
