edd6b3b88b376dfc68cc867bf441d0772a444d82
Minh Tran <tranminh@grinnell.edu>
2017-12-05 05:04:29 +0000

    change farm

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -3,4 +3,8 @@ class Farm < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+    has_many :operations, :class_name => "Operation"
+    has_many :growing_methods, :class_name => "GrowingMethod"
+    has_many :selling_methods, :class_name => "SellingMethod"
+    has_many :markets, :class_name => "Market"
 end
