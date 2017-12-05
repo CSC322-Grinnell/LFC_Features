3e9dfb8192fcbca15a5a3dfe2f5bbf8454f6f131
Minh Tran <tranminh@grinnell.edu>
2017-12-05 05:05:37 +0000

    merge

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -3,8 +3,16 @@ class Farm < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+<<<<<<< HEAD
+=======
+
+>>>>>>> Create associations
     has_many :operations, :class_name => "Operation"
     has_many :growing_methods, :class_name => "GrowingMethod"
     has_many :selling_methods, :class_name => "SellingMethod"
     has_many :markets, :class_name => "Market"
+<<<<<<< HEAD
+=======
+
+>>>>>>> Create associations
 end
