fb9db9b02d4569040e934a15b4793dcdd5fbeca4
linhpha <phamlinh@grinnell.edu>
2018-05-09 20:20:04 -0500

    Change migration

--- db/migrate/20180510005656_change_primary_operation_id_to_integer.rb
+++ db/migrate/20180510005656_change_primary_operation_id_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangePrimaryOperationIdToInteger < ActiveRecord::Migration[5.0]
    def change
-    change_column :farms, :primary_operation_id, :integer
+    change_column :farms, :primary_operation_id, 'integer USING CAST(primary_operation_id AS integer)'
   end
 end
