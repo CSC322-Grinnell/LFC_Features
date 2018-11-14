d9141094ffe7f09afdb0f20ec9abf9c26fe79dc3
Nolan Schoenle <schoenle@grinnell.edu>
2018-11-14 21:51:15 +0000

    Start outlining tests for farm

--- spec/models/farm_spec.rb
+++ spec/models/farm_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Farm, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#  pending "add some examples to (or delete) #{__FILE__}"
+    it "Creates a new farm" do
+        #Fill in this test 
+    end
 end
