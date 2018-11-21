6a6ab43bad408f2b9aaadafd2607c5dbff8f917f
Nolan Schoenle <schoenle@grinnell.edu>
2018-11-21 17:43:08 +0000

    Working on fixing the form test

--- spec/controllers/farm_controller_spec.rb
+++ spec/controllers/farm_controller_spec.rb
@@ -16,7 +16,7 @@ end
 
 RSpec.feature "Farm Signup", :type => :feature do
   scenario "User creates a new farm" do
-    visit "/farms"
+    visit "/farms" #Testing the form found in /views/farms/index.html.erb
 
     fill_in "Name", :with => "Test Farm"
     fill_in "Address", :with => "Test Farm"
@@ -25,6 +25,10 @@ RSpec.feature "Farm Signup", :type => :feature do
     fill_in "Facebook", :with => "Test Farm"
     fill_in "Instagram", :with => "Test Farm"
     fill_in "Twitter", :with => "Test Farm"
+    
+    #The following fields need to be added to the form.
+    fill_in "Email", :with => "testfarm@example.com"
+    fill_in "Password", :with => "1234567890"
     
     click_button "Submit"
 
