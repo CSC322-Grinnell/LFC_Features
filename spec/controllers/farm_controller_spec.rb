5a8e205fcc267bdade9c7f0830257189558e164e
Nolan Schoenle <schoenle@grinnell.edu>
2018-11-22 18:57:43 +0000

    Start writing test of invalid email entry

--- spec/controllers/farm_controller_spec.rb
+++ spec/controllers/farm_controller_spec.rb
@@ -15,8 +15,9 @@ end
 =end
 
 RSpec.feature "Farm Signup", :type => :feature do
-  scenario "User creates a new farm" do
-    visit "/farms" #Testing the form found in /views/farms/index.html.erb
+  #Testing the form found in /views/farms/index.html.erb
+  scenario "User creates a new farm properly" do
+    visit "/farms"
 
     fill_in "Name", :with => "Test Farm"
     fill_in "Address", :with => "1115 8th Ave Grinnell, IA 50112"
@@ -39,5 +40,31 @@ RSpec.feature "Farm Signup", :type => :feature do
     click_button "Submit"
 
     response.should redirect_to '/submission'
+  end
+  
+   scenario "User tries to create a farm without a valid email" do
+    visit "/farms" 
+
+    fill_in "Name", :with => "Test Farm"
+    fill_in "Address", :with => "1115 8th Ave Grinnell, IA 50112"
+    fill_in "Url", :with => "example.com"
+    fill_in "Phone", :with => "123-456-7890"
+    fill_in "Facebook", :with => "testFarm"
+    fill_in "Instagram", :with => "testFarm"
+    fill_in "Twitter", :with => "@testFarm"
+    fill_in "Email", :with => "123example.com"
+    fill_in "Password", :with => "1234567890"
+    
+=begin 
+  This test is failing because the Farm.create! method
+  requires market, growing_method, selling_methods, and 
+  operations to be a part of the farm as well. These will
+  either need to be in the form or fixed in the
+  farms_controller.rb
+  
+=end    
+    click_button "Submit"
+
+    #Figure out what we want to have it do when this scenario arises
   end
 end
