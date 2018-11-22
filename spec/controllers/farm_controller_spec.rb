33644127cff8b4d03cb23561a5d117e5cf44cc6d
Nolan Schoenle <schoenle@grinnell.edu>
2018-11-22 17:53:47 +0000

    Add comment about why test is failing to farm_controler_spec.rb

--- spec/controllers/farm_controller_spec.rb
+++ spec/controllers/farm_controller_spec.rb
@@ -19,17 +19,23 @@ RSpec.feature "Farm Signup", :type => :feature do
     visit "/farms" #Testing the form found in /views/farms/index.html.erb
 
     fill_in "Name", :with => "Test Farm"
-    fill_in "Address", :with => "Test Farm"
-    fill_in "Url", :with => "Test Farm"
-    fill_in "Phone", :with => "Test Farm"
-    fill_in "Facebook", :with => "Test Farm"
-    fill_in "Instagram", :with => "Test Farm"
-    fill_in "Twitter", :with => "Test Farm"
-    
-    #The following fields need to be added to the form.
+    fill_in "Address", :with => "1115 8th Ave Grinnell, IA 50112"
+    fill_in "Url", :with => "example.com"
+    fill_in "Phone", :with => "123-456-7890"
+    fill_in "Facebook", :with => "testFarm"
+    fill_in "Instagram", :with => "testFarm"
+    fill_in "Twitter", :with => "@testFarm"
     fill_in "Email", :with => "testfarm@example.com"
     fill_in "Password", :with => "1234567890"
     
+=begin 
+  This test is failing because the Farm.create! method
+  requires market, growing_method, selling_methods, and 
+  operations to be a part of the farm as well. These will
+  either need to be in the form or fixed in the
+  farms_controller.rb
+  
+=end    
     click_button "Submit"
 
     response.should redirect_to '/submission'
