7cec7eb49558e72913f2ccf468592bbcd20de69e
Joshua Cussen <cussenjo@grinnell.edu>
2018-11-27 17:51:00 +0000

    moved helper function that generates page titles to proper controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UiControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get ui_home_url
     assert_response :success
