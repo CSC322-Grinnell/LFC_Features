810ffde9b4014fdefeaee4856006e222cb9fdb44
Joshua Cussen <cussenjo@grinnell.edu>
2018-11-27 17:51:15 +0000

    some basic tests for static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Local Foods Connection"
+  end
+
+
   test "should get home" do
-    get ui_home_url
+    get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+    test "should get map" do
+    get static_pages_map_url
+    assert_response :success
+    assert_select "title", "Map | #{@base_title}"
   end
 
 end
