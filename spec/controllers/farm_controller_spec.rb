4464f2e8b0ddfe86f5305c77a7c002c4d92cc2a8
Nolan Schoenle <schoenle@grinnell.edu>
2018-11-27 02:30:23 +0000

    Add skeletons for some edit tests

--- spec/controllers/farm_controller_spec.rb
+++ spec/controllers/farm_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
-RSpec.feature "Farm Signup", :type => :feature do
+RSpec.feature "Farm signup exits", :type => :feature do
+  scenario "User goes directly to signup page" do
+    visit "/farms"
+   # expect(page).to be '/farms'
+  end
+  scenario "User clicks the signup link from home page" do
+    visit "/"
+    click_link "Sign Up" 
+    response.should redirect_to '/farms'
+  end
+
+end
+
+
+
+RSpec.feature "Farmer tries to sign up", :type => :feature do
   #Testing the form found in /views/farms/index.html.erb
   
   before (:each) do
@@ -52,6 +67,82 @@ RSpec.feature "Farm Signup", :type => :feature do
   end
 end
 
-RSpec.feature "Edit Farm", :type => :feature do
+RSpec.feature "Farm edit page can be reached", :type => :feature do
+  before (:each) do
+    f = Farm.create!(name: "Test2", 
+                  address: "123 St",
+                  password: "123456",
+                  phone: 3140614241,
+                  facebook: "facebook.com/test2",
+                  twitter: "twitter/farm2",
+                  created_at: "2018-011-18 20:45:00",
+                  updated_at: nil,
+                  approved: false,
+                  original_id: 1,
+                  email: "test2@example.com",
+                  contact_name: "Barb",
+                  year: 2018,
+                  statement: "She's your rich old granny",
+                  other_media: nil,
+                  link_to_cert: nil,
+                  growth_promoter: false,
+                  antibiotic: false,
+                  fav_activity: "",
+                  primary_operation_id: 2)
+  
+    @id = f.id
+  end
+  scenario "A farm's edit page exists when a farm is created" do
+    visit "/farms/edit/#{@id}"
+  end
+  
+  scenario "An unlogged in user clicks on the edit link" do
+    #Get the login page
+    #Check the login authentication
+    #Redirect to the farm's edit page
+    #Assert that we are on the edit page
+  end
+  
+  scenario "A logged in user clicks on the edit link" do
+    #Assert that the user is logged in
+    #Redirect to the user's farm's edit page
+    #Assert that we are on the correct page
+  end
+  
+  scenario "An unlogged in user attempts to access the edit page directly" do
+    #Get the farm's edit page
+    #Assert that an error is raised denying access
+  end
+
+end
+
+RSpec.feature "A valid user attempts to edit their farm", :type => :feature do
+  before (:each) do
+    f = Farm.create!(name: "Test2", 
+                  address: "123 St",
+                  password: "123456",
+                  phone: 3140614241,
+                  facebook: "facebook.com/test2",
+                  twitter: "twitter/farm2",
+                  created_at: "2018-011-18 20:45:00",
+                  updated_at: nil,
+                  approved: false,
+                  original_id: 1,
+                  email: "test2@example.com",
+                  contact_name: "Barb",
+                  year: 2018,
+                  statement: "She's your rich old granny",
+                  other_media: nil,
+                  link_to_cert: nil,
+                  growth_promoter: false,
+                  antibiotic: false,
+                  fav_activity: "",
+                  primary_operation_id: 2)
+  
+    @id = f.id
+  end
+  scenario "A logged in user edits a field" do
+  
+  end
 
 end
