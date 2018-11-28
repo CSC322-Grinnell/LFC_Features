85378213d61a75883afcd76313f57dbb7b3fbe27
Joshua Cussen <cussenjo@grinnell.edu>
2018-11-28 16:29:46 +0000

    added support for per-page hero image text

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -10,4 +10,14 @@ module StaticPagesHelper
     end
   end
 
+  # Returns the hero image text on a per-page basis.
+  def hero_text(text = '')
+    default_text = "Explore Local Farms"
+    if text.empty?
+      default_text
+    else
+      text
+    end
+  end
+
 end
