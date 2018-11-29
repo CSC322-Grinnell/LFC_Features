23faec94bb7ad72a382381e4c3b0d96354b2db9e
Joshua Cussen <cussenjo@grinnell.edu>
2018-11-29 15:58:14 +0000

    tidied up farm card creation

--- app/assets/javascripts/farm_api.js
+++ app/assets/javascripts/farm_api.js
@@ -1,39 +1,60 @@
 function handleIndexCall(result) {
-    document.getElementById("card-grid").innerHTML = "";
-    // set to new map
-    farms = {};
+
     for (var i = 0; i < result.length; i++) {
-        // create id
-        var id = "farm_" + result[i].id;
 
-        // add to map
-        //farms[id] = result[i];
-        // append card
-        $('#card-grid').append('<div class="card">' +
-            '<h3 class="card-name">' + result[i].name + '</h3><p>' +
-            '<span class="card-address">' + result[i].address + '</br>' +
-            //'<p class="card-text">CSA, Wholesale, and Farmers Market</p>' +
-            '<a href=tel:'+ result[i].phone + ' class="card-phone">' + result[i].phone + '</a> | ' +
-            '<a href=http://'+ result[i].url + ' class="card-website">' + result[i].url + '</a> ' +
-            '</p></div>'
-        );
-        // on click to show modal
-        //var isignore = false;
+        var cur_farm = result[i];
+        var id = "farm_" + cur_farm.id; // create id
 
-        $('#' + id).on('click', function() {
+        var farm_name = cur_farm.name;
+        var farm_address = cur_farm.address;
+        var farm_phone = cur_farm.phone;
+        var farm_url = cur_farm.url;
 
-            var new_id = this.getAttribute('id');
+        // create the farm card for the current farm
+        var farm_card = document.createElement("div");
+        $(farm_card).addClass("card");
 
-            // alter html in modal
-            //setAndShowFarmModal(farms[new_id]);
-        });
+        // if the farm has a name, add it to the card
+        if (farm_name.length > 0) {
+          var card_name = document.createElement("h3");
+          $(card_name).addClass("card-name");
+          card_name.append(farm_name);
+          farm_card.append(card_name);
+        }
 
-        $('.card-link').on('click', function(e) {
-            e.stopPropagation();
-        });
+        // if the farm has an address phone or url...
+        if (farm_address.length > 0 || farm_phone.length > 0 || farm_url.length > 0 ) {
+          // create a p element: card_info
+          var card_info = document.createElement("p");
+          $(card_info).addClass("card-info");
 
-        // add marker at proper placec
-        //geocodeAddressAndAddMarker(result[i]);
+          // if the farm has an address, add it to card_info
+          if (farm_address.length > 0) {
+            card_info.append(farm_address);
+          }
+
+          // if the farm has a phone, add it to card_info
+          if (farm_phone.length > 0) {
+            var card_phone = document.createElement("a");
+            card_phone.append(farm_phone);
+            $(card_phone).attr("href", 'tel:'+farm_phone); // add and format phone link to element
+            card_info.append(card_phone);
+          }
+
+          // if the farm has a url, add it to card_info
+          if (farm_url.length > 0) {
+            var card_url = document.createElement("a");
+            card_url.append(farm_url);
+            $(card_url).attr("href", farm_url);
+            card_info.append(card_url);
+          }
+
+          // add card_info to the farm_card
+          farm_card.append(card_info);
+        }
+
+        // add the farm_card to the grid
+        $('#card-grid').append(farm_card);
     }
 }
 
