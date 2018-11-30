847eaa29f489dfa511be9d76e89b0bd6b8d07f4f
Nolan Schoenle <schoenle@grinnell.edu>
2018-11-30 17:02:03 +0000

    Fix assertion for signup link test

--- spec/controllers/farm_controller_spec.rb
+++ spec/controllers/farm_controller_spec.rb
@@ -7,8 +7,8 @@ RSpec.feature "Farm signup exsists when", :type => :feature do
   end
   scenario "User clicks the signup link from home page" do
     visit "/"
-    click_link "Sign Up" 
-    response.should redirect_to '/farms/new'
+    click_link "Signup"
+    expect(page.current_path).to eq(new_farm_path)
   end
 
 end
@@ -35,8 +35,9 @@ RSpec.feature "Farmer tries to sign up", :type => :feature do
     fill_in "Email", :with => "testfarm@example.com"
     fill_in "Password", :with => "1234567890"
     
-    check :id =>'farm_market_1'
-
+    check('farm_operations_2')
+    #check :id =>'farm_operations_1'
+    
     click_button "Submit"
 
     expect(Farms.count).to eq(1)
