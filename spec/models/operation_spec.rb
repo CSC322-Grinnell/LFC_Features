ea78c634fedf6c2f7be123140fd47623b41d12fd
Hoang Cao <caohoang@grinnell.edu>
2018-12-07 16:13:02 +0000

    Testing for farm and operation

--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Operation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    operation_params = {food: "test"}
+    @testOperation = Operation.create!(operation_params)
+  end
+  
+  describe "new" do
+    it "should return an operation object" do
+      expect(@testOperation).to be_an_instance_of Operation
+    end
+  end
+  
+  describe "display_name" do
+    it "should display the right operation name" do
+      expect(@testOperation).display_name.to eql "test"
+    end
+  end
 end
