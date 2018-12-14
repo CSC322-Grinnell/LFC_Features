db707b23944ddf74f22bb7ab3aaab588bfcc7f4f
Nolan Schoenle <schoenle@grinnell.edu>
2018-12-14 00:03:58 +0000

    Add farm creation to home page feature. Card test  still isn't passing

--- spec/features/home_page_feature_spec.rb
+++ spec/features/home_page_feature_spec.rb
@@ -34,12 +34,19 @@ RSpec.feature "Home Page Features", :type => :feature do
   
   context "the home page responds as expected" do
     scenario "the search bar should filter the cards" do
+      #Create a CSA Farm
+      f = Farm.create!(name: "Compass Plant CSA", 
+                        address: "2039 N. Penrose Street. Grinnell, IA 50112",
+                        phone: '641-990-6832',
+                        email: 'ladunham@wildblue.net',
+                        password: 'password'
+      )
       #fill in some stuff
       fill_in "search-text-home" , :with => "CSA"
       #assert that it's what we want
       within(:css, 'div.card-grid') do 
-        expect(page).to have_css('div#farm_6')
-        #expect(page).to have_content('Compass Plant CSA')
+        #expect(page).to have_css('div#farm_6')
+        expect(page).to have_content('Compass Plant CSA')
       end
     end
     
