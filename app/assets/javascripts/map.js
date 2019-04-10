3c85e71a6f9c4f0af12a8779a8fe33aee731e984
Yuya Kawakami <kawakami@grinnell.edu>
2019-04-10 16:20:29 +0000

    Adding events support on map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -13,7 +13,7 @@ function geocodeAddressAndAddMarker(farm) {
     }, function(results, status) {
         // if OK/200 status, add marker to map, else throw alert.
         if (status === 'OK') {
-            addMarker(farm, results);
+            addFarmMarker(farm, results);
         }
         else {
             alert('Geocode was not successful for the following reason: ' + status);
@@ -30,18 +30,18 @@ var markerLastClicked
  farm: a json object of farm
  results: the return value of geocode function
  **/
-function addMarker(farm, results) {
+function addFarmMarker(farm, results) {
     // making a dictionary to store the information about the farm to print on sidebar
-    var infoDict = {}
-    
+    var farmInfoDict = {}
+
     // keys for dictionary
-    infoDict["name"]=farm.name;
-    infoDict["address"] = farm.address;
-    infoDict["phone"] = farm.phone;
-    infoDict["email"] = farm.email;
-    infoDict["facebook"] = farm.facebook;
-    infoDict["instagram"] = farm.instagram;
-    infoDict["twitter"] = farm.twitter;
+    farmInfoDict["name"] = farm.name;
+    farmInfoDict["address"] = farm.address;
+    farmInfoDict["phone"] = farm.phone;
+    farmInfoDict["email"] = farm.email;
+    farmInfoDict["facebook"] = farm.facebook;
+    farmInfoDict["instagram"] = farm.instagram;
+    farmInfoDict["twitter"] = farm.twitter;
 
     // for growing_methods, markets, operations, and selling methods iterate through the appropriate and collect relevant information
     var growing_methods = "";
@@ -63,30 +63,25 @@ function addMarker(farm, results) {
     for (var index = 0; index < farm.selling_methods.length; ++index) {
         selling_methods = selling_methods.concat(", ", farm.selling_methods[index].sell_method);
     }
-    
-    // keys for dictionary. str.slice(2) removes the ", " at the beginning of the string
-    infoDict["growing_methods"] = growing_methods.slice(2);
-    infoDict["markets"] = markets.slice(2);
-    infoDict["operations"] = operations.slice(2);
-    infoDict["selling_methods"] = selling_methods.slice(2);
-    
 
-    var farmAddress = farm.address
+    // keys for dictionary. str.slice(2) removes the ", " at the beginning of the string
+    farmInfoDict["growing_methods"] = growing_methods.slice(2);
+    farmInfoDict["markets"] = markets.slice(2);
+    farmInfoDict["operations"] = operations.slice(2);
+    farmInfoDict["selling_methods"] = selling_methods.slice(2);
+    
     var marker = new google.maps.Marker({
         map: map,
         position: results[0].geometry.location,
-        title: farm.name,
         // icon: "https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png"
         icon: "/assets/farm_icon.png"
     });
-    
-   
-    
+
     // add listener to map for marker to display info window
     google.maps.event.addListener(marker, 'click', function() {
         // open info window
-        openSidebar();
-        addFarminfotoSidebar(infoDict);
+        openSidebar("farmSidebar");
+        addFarminfotoSidebar(farmInfoDict);
         // if there is a marker that's clicked previously set the icon back to farm_icon
         if (markerLastClicked) {
             markerLastClicked.setIcon("/assets/farm_icon.png");
@@ -97,31 +92,94 @@ function addMarker(farm, results) {
     });
     markers.push(marker);
 }
+
+var eventArray = [
+    {eventName: "Community Meal", position: { lat: 41.737665, lng: -92.725401 }, time: "Every Tuesday", location: "Davis Elementary School" },
+    {eventName: "Grinnell Farm to Table", position: { lat: 41.745446, lng: -92.721348 }, time: "Every Thursday", location: "First Presbyterian Church" }
+]
+
+function addEventMarker(event) {
+
+    var marker = new google.maps.Marker({
+        map: map,
+        position: event.position,
+        // icon: "https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png"
+        icon: "/assets/farm_icon.png"
+    });
+
+
+    // add listener to map for marker to display info window
+    google.maps.event.addListener(marker, 'click', function() {
+        // open info window
+        openSidebar("eventSidebar");
+        console.log(event);
+        addEventinfotoSidebar(event);
+        // if there is a marker that's clicked previously set the icon back to farm_icon
+        if (markerLastClicked) {
+            markerLastClicked.setIcon("/assets/farm_icon.png");
+        }
+        // change the marker icon to highlighted_farm_icon and store current marker to markerLastClicked
+        marker.setIcon("/assets/highlighted_farm_icon.png");
+        markerLastClicked = marker;
+    });
+    markers.push(marker);
+
+}
+
 /**
  * open sidebar to show farm information
  **/
- 
-function openSidebar() {
+
+function openSidebar(sidebarType) {
+    // Close all sidebars first
+    var sidebarArray = document.getElementsByClassName("col-md-3");
+    for (var i = 0; i < sidebarArray.length; i++) {
+        sidebarArray[i].style.display = "none";
+    }
     // To open the sidebar resize the map, then make the sidebar visible
     document.getElementById("mapContainer").classList.remove("col-md-12");
     document.getElementById("mapContainer").classList.add("col-md-9");
-    document.getElementById("sidebar").style.display = "block";
+    sidebarArray = document.getElementsByClassName("col-md-3");
+    for (var i = 0; i < sidebarArray.length; i++) {
+        if (sidebarArray[i].id == sidebarType) {
+            sidebarArray[i].style.display = "block";
+        }
+    }
 }
+
+/**
+ * close the sidebar
+ **/
+
+function closeSidebar(sidebarType) {
+    document.getElementById("mapContainer").classList.remove("col-md-9");
+    document.getElementById("mapContainer").classList.add("col-md-12");
+    var sidebarArray = document.getElementsByClassName("col-md-3");
+    for (var i = 0; i < sidebarArray.length; i++) {
+        if (sidebarArray[i].id == sidebarType) {
+            sidebarArray[i].style.display = "none";
+        }
+    }
+    markerLastClicked.setIcon("/assets/farm_icon.png");
+}
+
+
+
 /**
  * add the farm information to the sidebar
  * @param
- * infoDict: dictionary that holds the farm information
+ * farmInfoDict: dictionary that holds the farm information
  **/
- 
-function addFarminfotoSidebar(infoDict) {
+
+function addFarminfotoSidebar(farmInfoDict) {
     // iterate through the key-value pairs
-    for (var key in infoDict) {
-        var value = infoDict[key];
+    for (var key in farmInfoDict) {
+        var value = farmInfoDict[key];
         // if there is something stored in value (i.e. neither null nor empty) then make sure that the element will display and add information
-        if (value){
+        if (value) {
             document.getElementById(key).style.display = '';
             document.getElementById(key.concat("-info")).textContent = value;
-        } 
+        }
         // if there's nothing to show make it invisible
         else {
             document.getElementById(key).style.display = 'none';
@@ -130,15 +188,32 @@ function addFarminfotoSidebar(infoDict) {
 }
 
 /**
- * close the sidebar
+ * add the farm information to the sidebar
+ * @param
+ * farmInfoDict: dictionary that holds the farm information
  **/
- 
-function closeSidebar() {
-    document.getElementById("mapContainer").classList.remove("col-md-9");
-    document.getElementById("mapContainer").classList.add("col-md-12");
-    document.getElementById("sidebar").style.display = "none";
-    markerLastClicked.setIcon("/assets/farm_icon.png");
+
+function addEventinfotoSidebar(eventInfoDict) {
+    // iterate through the key-value pairs
+    for (var key in eventInfoDict) {
+        if (key == "position"){
+            continue;
+        }
+        var value = eventInfoDict[key];
+        console.log(value);
+        // if there is something stored in value (i.e. neither null nor empty) then make sure that the element will display and add information
+        if (value) {
+            document.getElementById(key).style.display = '';
+            document.getElementById(key.concat("-info")).textContent = value;
+        }
+        // if there's nothing to show make it invisible
+        else {
+            document.getElementById(key).style.display = 'none';
+        }
+    }
 }
+
+
 
 /**
  show markers on the map
@@ -146,10 +221,13 @@ function closeSidebar() {
  results: an array of farm jsons
  **/
 function showMarkers(result) {
-    console.log(result);
     for (var i = 0; i < result.length; i++) {
         // add marker at proper place
         geocodeAddressAndAddMarker(result[i]);
+    }
+    for (var i = 0; i < eventArray.length; i++) {
+        // add marker at proper place
+        addEventMarker(eventArray[i]);
     }
 }
 
@@ -190,73 +268,73 @@ function initMap() {
     callIndexApi(call_url, showMarkers)
 
 
-    var contentString = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h1 id="firstHeading" class="firstHeading">Grinnell, IA</h1>' +
-        '<div id="bodyContent">' +
-        '<p><b>Grinnell</b> is a city in Poweshiek County, Iowa, United States.' +
-        'The population was 9,218 at the 2010 census.</p>' +
-        '<p>Attribution: Grinnell, <a href="https://en.wikipedia.org/wiki/Grinnell,_Iowa">' +
-        'https://en.wikipedia.org/wiki/Grinnell,_Iowa</a> ' +
-        '(last visited Feb 25, 2019).</p>' +
-        '</div>' +
-        '</div>';
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
+    // var contentString = '<div id="content">' +
+    //     '<div id="siteNotice">' +
+    //     '</div>' +
+    //     '<h1 id="firstHeading" class="firstHeading">Grinnell, IA</h1>' +
+    //     '<div id="bodyContent">' +
+    //     '<p><b>Grinnell</b> is a city in Poweshiek County, Iowa, United States.' +
+    //     'The population was 9,218 at the 2010 census.</p>' +
+    //     '<p>Attribution: Grinnell, <a href="https://en.wikipedia.org/wiki/Grinnell,_Iowa">' +
+    //     'https://en.wikipedia.org/wiki/Grinnell,_Iowa</a> ' +
+    //     '(last visited Feb 25, 2019).</p>' +
+    //     '</div>' +
+    //     '</div>';
+    // var infowindow = new google.maps.InfoWindow({
+    //     content: contentString
+    // });
 
-    var marker = new google.maps.Marker({
-        position: grinnell,
-        map: map,
-        title: 'Grinnell (Jewel of the Prairie)'
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
+    // var marker = new google.maps.Marker({
+    //     position: grinnell,
+    //     map: map,
+    //     title: 'Grinnell (Jewel of the Prairie)'
+    // });
+    // marker.addListener('click', function() {
+    //     infowindow.open(map, marker);
+    // });
 
-    var community_meal = { lat: 41.737665, lng: -92.725401 };
+    // var community_meal = { lat: 41.737665, lng: -92.725401 };
 
-    var content_cm = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h2>Community Meal</h2>' + '<p><b>Time:</b> Every Tuesday</p>' +
-        '<p><b>Where:</b> Davis Elementary School</p>' +
-        '</div>';
+    // var content_cm = '<div id="content">' +
+    //     '<div id="siteNotice">' +
+    //     '</div>' +
+    //     '<h2>Community Meal</h2>' + '<p><b>Time:</b> Every Tuesday</p>' +
+    //     '<p><b>Where:</b> Davis Elementary School</p>' +
+    //     '</div>';
 
-    var infowindow_cm = new google.maps.InfoWindow({
-        content: content_cm
-    });
+    // var infowindow_cm = new google.maps.InfoWindow({
+    //     content: content_cm
+    // });
 
-    var marker_cm = new google.maps.Marker({
-        position: community_meal,
-        map: map,
-        title: 'Community Meal'
-    });
-    marker_cm.addListener('click', function() {
-        infowindow_cm.open(map, marker_cm);
-    });
+    // var marker_cm = new google.maps.Marker({
+    //     position: community_meal,
+    //     map: map,
+    //     title: 'Community Meal'
+    // });
+    // marker_cm.addListener('click', function() {
+    //     infowindow_cm.open(map, marker_cm);
+    // });
 
-    var farm_to_table = { lat: 41.745446, lng: -92.721348 };
+    // var farm_to_table = { lat: 41.745446, lng: -92.721348 };
 
-    var content_ft = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h2>Grinnell Farm to Table</h2>' + '<p><b>Time:</b> Every Thursday</p>' +
-        '<p><b>Where:</b> First Presbyterian Church</p>' +
-        '</div>';
+    // var content_ft = '<div id="content">' +
+    //     '<div id="siteNotice">' +
+    //     '</div>' +
+    //     '<h2>Grinnell Farm to Table</h2>' + '<p><b>Time:</b> Every Thursday</p>' +
+    //     '<p><b>Where:</b> First Presbyterian Church</p>' +
+    //     '</div>';
 
-    var infowindow_ft = new google.maps.InfoWindow({
-        content: content_ft
-    });
+    // var infowindow_ft = new google.maps.InfoWindow({
+    //     content: content_ft
+    // });
 
-    var marker_ft = new google.maps.Marker({
-        position: farm_to_table,
-        map: map,
-        title: 'Grinnell Farm to Table'
-    });
-    marker_ft.addListener('click', function() {
-        infowindow_ft.open(map, marker_ft);
-    });
+    // var marker_ft = new google.maps.Marker({
+    //     position: farm_to_table,
+    //     map: map,
+    //     title: 'Grinnell Farm to Table'
+    // });
+    // marker_ft.addListener('click', function() {
+    //     infowindow_ft.open(map, marker_ft);
+    // });
 
 }
