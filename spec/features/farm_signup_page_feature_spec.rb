cf78bbe65cf6b85a5e48d028c8d1bfeb35ecd670
HongyuanZhang <zhanghon@grinnell.edu>
2019-05-14 19:02:02 -0500

    added a lot of tests (missing input, invalid input, valid input)

--- spec/features/farm_signup_page_feature_spec.rb
+++ spec/features/farm_signup_page_feature_spec.rb
@@ -3,66 +3,82 @@ require 'rails_helper'
 RSpec.feature "Farm signup exsists when", :type => :feature do
   scenario "User goes directly to signup page" do
     visit '/signup'
-   # expect(page).to be '/farms/new'
+    expect(page.current_path).to eq('/signup')
   end
   scenario "User clicks the signup link from home page" do
     visit "/"
     click_link "Sign up"
     expect(page.current_path).to eq(new_farm_path)
   end
-
 end
-
-
 
 RSpec.feature "Farmer tries to sign up", :type => :feature do
   #Testing the form found in /views/farms/new.html.erb
-
   before (:each) do
     visit "/farms/new"
-
-    fill_in "Name", :with => "Test Farm"
-    fill_in "Address", :with => "1115 8th Ave Grinnell, IA 50112"
-    fill_in "Url", :with => "example.com"
-    fill_in "Phone", :with => "123-456-7890"
-    fill_in "Facebook", :with => "testFarm"
-    fill_in "Instagram", :with => "testFarm"
-    fill_in "Twitter", :with => "@testFarm"
   end
   
-  scenario " with valid input" do
-    farm_count_initial = Farm.count
-    
+  scenario " with missing name" do
     fill_in "Email", :with => "testfarm@example.com"
     fill_in "Password", :with => "1234567890"
-
-    save_and_open_page
-    #page.check("farm_operations_1")
-    find('farm_operations_1', :text => 'egg').click
-    
     click_button "Submit"
-    
-    farm_count_final = Farm.count
-
-    expect(farm_count_final - farm_count_initial).to eq(1)
-    #response.should redirect_to '/submission' #Not yet working on the farms/new
+    expect(page).to have_content('Name can\'t be blank')
+  end
+  
+  scenario "with an missing email" do
+    fill_in "Name", :with => "Test Farm"
+    fill_in "Password", :with => "1234567890"
+    click_button "Submit"
+    expect(page).to have_content('Email can\'t be blank')
+  end
+  
+  scenario "with an missing password" do
+    fill_in "Name", :with => "Test Farm"
+    fill_in "Email", :with => "testfarm@example.com"
+    click_button "Submit"
+    expect(page).to have_content('Password can\'t be blank')
   end
   
   scenario "with an invalid email" do
-    fill_in "Email", :with => "123example.com" #an invalid email
+    fill_in "Name", :with => "Test Farm"
+    fill_in "Email", :with => "testfarm.com"
     fill_in "Password", :with => "1234567890"
-    expect {click_button "Submit"}.to raise_error
-    
-
-    #Figure out what we want to have it do when this scenario arises
+    click_button "Submit"
+    expect(page).to have_content('Email is invalid')
   end
   
   scenario "with an invalid password" do
+    fill_in "Name", :with => "Test Farm"
     fill_in "Email", :with => "testfarm@example.com"
     fill_in "Password", :with => "123"
-    
-    expect {click_button "Submit"}.to raise_error
-    
-    #Figure out what we want to have it do when this scenario arises
+    click_button "Submit"
+    expect(page).to have_content('Password is too short (minimum is 6 characters)')
+  end
+  
+  scenario "with password confirmation not matching password" do
+    fill_in "Name", :with => "Test Farm"
+    fill_in "Email", :with => "testfarm@example.com"
+    fill_in "Password", :with => "123456"
+    fill_in "Password confirmation", :with => "123455"
+    click_button "Submit"
+    expect(page).to have_content('Password confirmation doesn\'t match Password')
+  end
+  
+  scenario "with password confirmation not matching password" do
+    fill_in "Name", :with => "Test Farm"
+    fill_in "Email", :with => "testfarm@example.com"
+    fill_in "Password", :with => "123456"
+    fill_in "Password confirmation", :with => "123455"
+    click_button "Submit"
+    expect(page).to have_content('Password confirmation doesn\'t match Password')
+  end
+  
+  scenario "with valid input" do
+    fill_in "Name", :with => "Test Farm"
+    fill_in "Email", :with => "testfarm@example.com"
+    fill_in "Password", :with => "123456"
+    fill_in "Password confirmation", :with => "123456"
+    click_button "Submit"
+    expect(page.current_path).to eq('/farms/1')
   end
 end
