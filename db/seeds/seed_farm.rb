36d28178a878e95cd7e0f459adbd0ef73434267f
NateWilliams2 <Williams2@grinnell.edu>
2019-10-09 18:01:12 -0500

    Fix typo

--- db/seeds/seed_farm.rb
+++ db/seeds/seed_farm.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Farm.create(name: 'admin', email: 'admin@example.com', password: 'password',
+Farm.create!(name: 'admin', email: 'admin@example.com', password: 'password',
              password_confirmation: 'password', role: 1) 
             # if Rails.env.development?
 
