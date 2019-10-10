463bb2e3b51fd899b93f481382f96dff1abf61b6
NateWilliams2 <Williams2@grinnell.edu>
2019-10-10 15:50:15 -0500

    Enable admin controls for growing method, selling method, market, operation

--- app/admin/markets.rb
+++ app/admin/markets.rb
@@ -1,15 +1,38 @@
 ActiveAdmin.register Market do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
-end
+    # See permitted parameters documentation:
+    # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+    #
+    # permit_params :list, :of, :attributes, :on, :model
+    #
+    # or
+    #
+    # permit_params do
+    #   permitted = [:permitted, :attributes]
+    #   permitted << :other if params[:action] == 'create' && current_user.admin?
+    #   permitted
+    # end
+      ## Add strong parameters for admin to be able to edit these fields
+      permit_params :location, :market
+    
+      # Specifies how the farms should be presented to the admin on the page /admin/farms
+      # (this is the main table on the left)
+      index do
+        # selectable_column # commented out because there doesn't seem to be any purpose in selecting multiple farms
+        id_column
+        column :location
+        actions
+      end
+    
+      # Adds name and email options to the filter box at the right side of the page
+      # The filter box is used to filter the table defined above in `index do ...`
+      filter :location
+    
+      # Specifies the format of the view/edit farm page
+      # eg /admin/farms/7 or/admin/farms/7/edit
+      form do |f|
+        f.inputs "Market" do
+          f.input :location
+        end
+        f.actions
+      end
+    end
