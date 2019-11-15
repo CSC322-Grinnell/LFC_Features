87f21c6ba876e33beca209b0b4f5c93402349b13
Shuyi Qi <qishuyi0525@gmail.com>
2019-11-14 22:08:36 -0600

    Restore configs for simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,3 +94,9 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+require 'capybara/rspec' 
+
+require 'simplecov'
+require 'simplecov-console'
+SimpleCov.formatter = SimpleCov::Formatter::Console
+SimpleCov.start
