22247845d8c4e2e2a9e0a91d5b3e3076ea84acc9
NateWilliams2 <Williams2@grinnell.edu>
2019-11-24 17:11:49 -0600

    Attempt to resolve db conflicts

--- db/migrate/20180510005656_change_primary_operation_id_to_integer.rb
+++ db/migrate/20180510005656_change_primary_operation_id_to_integer.rb
@@ -1,10 +1,10 @@
 class ChangePrimaryOperationIdToInteger < ActiveRecord::Migration[5.0]
-  #   def change
+  #   def self.up
   #     change_column :farms, :primary_operation_id, 'integer USING CAST(primary_operation_id AS integer)'
   #   end
 
   def self.up
-    change_column :farms, :primary_operation_id, :integer
+    change_column :farms, :primary_operation_id, :integer, using: "primary_operation_id::integer"
   end
 
   def self.down
