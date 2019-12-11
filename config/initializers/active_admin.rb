c6c4bff27387d2361add0502273811c3258a1436
NateWilliams2 <Williams2@grinnell.edu>
2019-12-11 11:43:52 -0600

    Clean up admin portal

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -32,7 +32,7 @@ ActiveAdmin.setup do |config|
   # To set no namespace by default, use:
   #   config.default_namespace = false
   #
-  # 
+  #
   #
   # You can customize the settings for each namespace by using
   # a namespace block. For example, to change the site title
